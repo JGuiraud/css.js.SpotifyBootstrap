d4be26577eb4d3ea4a30d4e5d7b2f86a60341526
JGuiraud <jerome31guiraud@gmail.com>
2017-03-03 16:13:30 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+
+    console.log("ready");
+    var app = {
+        url: null,
+        pseudo: null,
+        password: null,
+        connexion: [],
+        init: function () {
+            app.listener()
+            app.url = "../data/users.json"
+        },
+        listener: function () {
+            console.log("coucou")
+            $("#btnsend").on("click", function (e) {
+                e.preventDefault();
+                app.getUsers(app.url);
+            })
+        },
+        getUsers: function (url) {
+            $.ajax({
+                url: url,
+                success: app.connect,
+                error: function () { if (err) console(err) }
+            })
+        },
+        connect: function (data) {
+            app.pseudo = $("#pseudo").val()
+            app.password = $("#pass").val()
+            app.connexion.push({ username: app.pseudo, password: app.password })
+            console.log(app.connexion)
+            data.Users.map(function (i) {
+                console.log(i)
+                if (i.username === app.pseudo && i.password === app.password) {
+                    console.log(i.color)
+                    app.append(i.color);
+                } else {
+                    console.log("tg")
+                }
+            })
+        },
+        append: function (color) {
+            $("*").css("color", color)
+        }
+    }
+    app.init()
+
+});
